3c4acefbd0723c224fda63d6f489d8fe76a548d7
nyan <1eaegue@gmail.com>
2018-05-08 19:22:10 +0300

    implement the parse function

--- src/chrono/core.cljc
+++ src/chrono/core.cljc
@@ -1,5 +1,17 @@
 (ns chrono.core
-  (:require [chrono.util :as util]))
+  (:require [chrono.util :as util]
+            [clojure.string :as str]))
+
+(def keys-map
+  {"%Y" :year
+   "%m" :month
+   "%d" :day
+   "%H" :hour
+   "%M" :minute
+   "%S" :second})
+
+(def formats
+  {:iso "%Y-%m-%dT%H:%M:%S"})
 
 (defn time [t]
   (merge {:type :time
@@ -8,7 +20,20 @@
           :day 1
           :tz :utc} t))
 
-(defn parse [s format])
+(defn parse [s f]
+  (cond
+    (keyword? f)
+    (parse s (get formats f))
+
+    :else
+    (let [re-f (re-pattern (str/replace f #"%." #(get {"%Y" "(\\d{4})"}
+                                                      %
+                                                      "(\\d{2})")))
+          values (rest (re-find re-f s))
+          patterns (re-seq #"%." f)]
+      (zipmap (map keys-map patterns)
+              (map (fn [n] #?(:clj (Integer. n)
+                              :cljs (int n))) values)))))
 
 (defn format [t format])
 
--- test/chrono/core_test.clj
+++ test/chrono/core_test.clj
@@ -1,24 +1,24 @@
 (ns chrono.core-test
   (:require [clojure.test :refer :all]
-            [chrono.core :refer :all]))
+            [chrono.core :as sut]))
 
 (deftest add-test
   (testing "intervals"
-    (is (= (add {:type :interval
-                 :second 321}
-                {:minute 58})
-           {:type :time
+    (is (= (sut/add {:type :interval
+                     :second 321}
+                    {:minute 58})
+           {:type :interval
             :hour 1
             :minute 3
             :second 21})))
 
   (testing "time"
-    (is (= (add (time {:year 1999
-                       :month 6
-                       :day 28})
-                {:hour 24
-                 :month 8
-                 :minute 10})
+    (is (= (sut/add (sut/time {:year 1999
+                               :month 6
+                               :day 28})
+                    {:hour 24
+                     :month 8
+                     :minute 10})
            {:type :time
             :tz :utc
             :year 2000
@@ -29,5 +29,15 @@
             :second 0})))
 
   (testing "raw intervals"
-    (is (= (add {:minute 40} {:minute 30})
+    (is (= (sut/add {:minute 40} {:minute 30})
            {:minute 70}))))
+
+(deftest parse-test
+  (is (= (sut/parse "2000-03-23T19:44:03" "%Y-%m-%dT%H:%M:%S")
+         (sut/parse "2000-03-23T19:44:03" :iso)
+         {:year 2000
+          :month 3
+          :day 23
+          :hour 19
+          :minute 44
+          :second 3})))
